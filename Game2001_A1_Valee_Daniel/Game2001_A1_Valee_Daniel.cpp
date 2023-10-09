9f8c0939afa88e9edf205cee81abf280e6bbe343
Slayer-Quanta <91901675+Slayer-Quanta@users.noreply.github.com>
2023-10-08 23:07:03 -0400

    added explanations in main to help understand my program which i think follows the little instructions you put on the dropox

--- Game2001_A1_Valee_Daniel/Game2001_A1_Valee_Daniel.cpp
+++ Game2001_A1_Valee_Daniel/Game2001_A1_Valee_Daniel.cpp
@@ -6,31 +6,45 @@
 #include "Array.h"
 
 int main() {
+    // Create an ordered array with a capacity of 4 and prevent duplicates
     OrderedArray ordered_array(4, true);
-    ordered_array.push(5);
-    ordered_array.push(3);
-    ordered_array.push(7);
-    ordered_array.push(3);
-    cout << "Ordered Array: ";
-    ordered_array.display();  
 
+    // Push some values to the ordered array to see it grow
+    ordered_array.push(5); // Array expands when it gets full (initial capacity: 4)
+    ordered_array.push(3); // It grows to handle more items (capacity: 8)
+    ordered_array.push(7); // Keeps expanding as needed (capacity: 16)
+    ordered_array.push(3); // Ignores duplicates
+
+    // Show the ordered array
+    cout << "Ordered Array: ";
+    ordered_array.display();
+
+    // Check if a value is in the ordered array
     int search_item = 5;
     if (ordered_array.contains(search_item)) {
-        cout << search_item << " is in the ordered array." << endl;
+        cout << "Yep, " << search_item << " is in there." << endl;
     }
     else {
-        cout << search_item << " is not in the ordered array." << endl;
+        cout << "Nope, " << search_item << " didn't make it." << endl;
     }
-    
-    UnorderedArray unordered_array(4); 
+
+    // Create an unordered array with a capacity of 4
+    UnorderedArray unordered_array(4);
+
+    // Put some numbers into the unordered array
     unordered_array.push(9);
     unordered_array.push(2);
-    unordered_array.push(9);  
-    cout << "Unordered Array: ";
-    unordered_array.display();  
+    unordered_array.push(9); // Duplicates are okay here
 
+    // Display the unordered array
+    cout << "Unordered Array: ";
+    unordered_array.display();
+
+    // Take some values out of the unordered array
     int popped_item1 = unordered_array.pop();
     int popped_item2 = unordered_array.pop();
+
+    // Display the items we took out
     cout << "Popped items from Unordered Array: " << popped_item1 << " " << popped_item2 << endl;
 
     return 0;
